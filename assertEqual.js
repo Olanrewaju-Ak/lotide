8376f3adc752da7a26ca8dbad6fddbb0492ea7cc
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-05-22 09:14:43 -0700

    Emoji added to assertion message

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,13 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("Assertion passed : " + actual + " === " + expected);
+    console.log("✅✅  Assertion passed : " + actual + " === " + expected);
   } else {
-    console.log("Assertion failed : " + actual + " !== " + expected);
+    console.log("❌❌ Assertion failed : " + actual + " !== " + expected);
   }
 };
 
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
+assertEqual("food", "food");
+assertEqual(3, 1);
