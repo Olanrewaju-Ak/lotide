7a6ba25182d30ac5d800c96ed90424f11222bd1f
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-05-24 01:00:25 -0700

    countOnly function works

--- countOnly.js
+++ countOnly.js
@@ -6,6 +6,7 @@ const assertEqual = function (actual, expected) {
   }
 };
 
+/*
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
 const countOnly = function (allItems, itemsToCount) {
@@ -14,21 +15,38 @@ const countOnly = function (allItems, itemsToCount) {
     if (itemsToCount[key] === true) {
       console.log(itemsToCount[key]);
     } else {
-      console.log(Object.keys(itemsToCount));
+      console.log(key);
     }
   }
   // check to see the count of each item in the array;
   for (let i = 0; i < allItems.length; i++) {
-    console.log(allItems[i]);
+    // console.log(allItems[i]);
     let count = 0;
     allItems.forEach((item) => {
       //trying to check how many times each item occurs in the array allItems
       if (item === allItems[i]) {
         count++;
       }
-      console.log(i + "is " + count);
+      // console.log(i + "is " + count);
     });
   }
+};
+
+*/
+
+const countOnly = (allItems, itemsToCount) => {
+  const results = {};
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1; //if it sees that the item already exist in the object result, it will increase the number by 1
+      } else {
+        results[item] = 1; //this will initially set the value of the key to 1
+      }
+    }
+  }
+  console.log(results);
+  return results;
 };
 
 const firstNames = [
