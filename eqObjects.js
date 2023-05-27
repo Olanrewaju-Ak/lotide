54055e9c34e4044ab1ba3ba9390b666c3c84f2d1
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-05-27 07:38:18 -0700

    eqObjects

---
+++ eqObjects.js
@@ -0,0 +1,7 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅ Assertion passed : ${actual}  === ${expected}`);
+  } else {
+    console.log(`❌❌ Assertion failed : ${actual} !== ${expected}`);
+  }
+};
