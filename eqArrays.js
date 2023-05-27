049720789a35096c9c7738ee6a8778ce4ab224bd
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-05-27 08:39:17 -0700

    first half of eqObjects done

--- eqArrays.js
+++ eqArrays.js
@@ -11,21 +11,33 @@ const assertEqual = function (actual, expected) {
 //also compare the indices if they are equal.
 //if the two conditions checkout, then assertqual should return true.
 
+// const eqArrays = function (array1, array2) {
+//   let notInBoth = [];
+//   for (const item of array1) {
+//     if (array2.includes(item) && array1.indexOf(item) === array2.indexOf(item)) {
+//     } else {
+//       notInBoth.push(item);
+//     }
+//   }
+//   if (notInBoth.length) {
+
+//     return false;
+//   } else {
+
+//     return true;
+//   }
+// };
+
 const eqArrays = function (array1, array2) {
-  let notInBoth = [];
-  for (const item of array1) {
-    if (array2.includes(item) && array1.indexOf(item) === array2.indexOf(item)) {
-    } else {
-      notInBoth.push(item);
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
     }
   }
-  if (notInBoth.length) {
-    // console.log(false);
-    return false;
-  } else {
-    // console.log(true);
-    return true;
-  }
+  return true;
 };
 
 assertEqual(eqArrays(["1", "2", "3"], ["3", "2", 1]), false);
--- eqObjects.js
+++ eqObjects.js
@@ -5,3 +5,51 @@ const assertEqual = function (actual, expected) {
     console.log(`❌❌ Assertion failed : ${actual} !== ${expected}`);
   }
 };
+
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const eqObjects = function (object1, object2) {
+  let keysObj1 = Object.keys(object1);
+  let keysObj2 = Object.keys(object2);
+
+  if (keysObj1.length !== keysObj2.length) {
+    return false;
+  } else {
+    for (const key of keysObj1) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+  }
+};
+
+//TESTS
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+assertEqual(eqObjects(shirtObject, anotherShirtObject), true); // => true
+
+const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false); // => false
+
+const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true); // => true
+
+const longSleeveMultiColorShirtObject = {
+  size: "medium",
+  colors: ["red", "blue"],
+  sleeveLength: "long"
+};
+assertEqual(eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject), false); // => false
