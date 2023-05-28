9f78739c410412b1fb5b0f3ba8032e1a313a2336
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-05-28 13:04:50 -0700

    findkey fuction works now

---
+++ findkey.js
@@ -0,0 +1,43 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅ Assertion passed : ${actual}  === ${expected}`);
+  } else {
+    console.log(`❌❌ Assertion failed : ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function (object, callBack) {
+  for (const key in object) {
+    if (callBack(object[key])) {
+      console.log(key);
+      return key;
+    }
+  }
+};
+
+findKey(
+  {
+    "Blue Hill": { stars: 1 },
+    Akaleri: { stars: 3 },
+    noma: { stars: 2 },
+    elBulli: { stars: 3 },
+    Ora: { stars: 2 },
+    Akelarre: { stars: 3 }
+  },
+  (x) => x.stars === 2
+); // => "noma"
+
+assertEqual(
+  findKey(
+    {
+      "Blue Hill": { stars: 1 },
+      Akaleri: { stars: 3 },
+      noma: { stars: 2 },
+      elBulli: { stars: 3 },
+      Ora: { stars: 2 },
+      Akelarre: { stars: 3 }
+    },
+    (x) => x.stars === 2
+  ),
+  "noma"
+); // => "noma"
