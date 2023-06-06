3a2f0fb2cb9d20df856e4ba1b0a47a8b5140441b
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-06-06 13:13:13 -0700

    tail,assertEqual and head refactored, test files created separately

--- head.js
+++ head.js
@@ -1,16 +1,6 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅ Assertion passed : ${actual}  === ${expected}`);
-  } else {
-    console.log(`❌❌ Assertion failed : ${actual} !== ${expected}`);
-  }
-};
-
 const head = function (array) {
   let firstItem = array[0];
   return array && array.length ? firstItem : undefined;
 };
 
-// TEST CODE
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
--- tail.js
+++ tail.js
@@ -1,25 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅ Assertion passed : ${actual}  === ${expected}`);
-  } else {
-    console.log(`❌❌ Assertion failed : ${actual} !== ${expected}`);
-  }
-};
-
-const tail = function(array) {
+const tail = function (array) {
   let headless = [...array].splice(1);
   return array && array.length ? headless : undefined;
 };
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-const fan = tail(["chelsea"]);
-assertEqual(fan.length, 0);
+module.exports = tail;
