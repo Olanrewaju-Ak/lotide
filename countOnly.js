f26054cb788844523f828e55ae59bd424f653e6e
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-06-07 16:47:07 -0700

    removed commented out code

--- countOnly.js
+++ countOnly.js
@@ -1,20 +1,3 @@
-// Solution 1
-// const countOnly = (allItems, itemsToCount) => {
-//   const results = {};
-//   for (const item of allItems) {
-//     if (itemsToCount[item]) {
-//       console.log(itemsToCount[item]);
-//       if (results[item]) {
-//         results[item] += 1; //if it sees that the item already exist in the object result, it will increase the number by 1
-//       } else {
-//         results[item] = 1; //this will initially set the value of the key to 1
-//       }
-//     }
-//   }
-//   console.log(results);
-//   return results;
-// };
-
 const countOnly = (allItems, itemsToCount) => {
   const results = {};
   for (const item of allItems) {
